174dab667c3bd12c1ba0ac5213e24c073e398c61
ogr <ogr@ehtrace.com>
2023-09-15 12:11:38 +0200

    options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-export const generate = (input: string) => {
-  console.log(input, "toto");
-  return input;
-};
+/**
+ * typoRate   - Taux d'erreur de faute de frappe  - par défaut 0.1 // TODO
+ * runs       - Nombre de passes                  - par défaut 1   // TODO
+ * insanity   - Taux de folie                     - par défaut 0.3 // TODO
+ */
+export type Options = {
+  typoRate: number
+  runs: number
+  insanity: number
+}
+
+export const generate = (input: string, options?: Options) => {
+  console.log(input, 'toto')
+  return input
+}
